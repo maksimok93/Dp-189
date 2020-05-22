7b0b69f4c154bfbf9bbd6f2ec3c8930aa0dfa3eb
Maksym Bezruchko <maksimok.dnepr@gmail.com>
2020-05-22 05:56:25 +0300

    last commit

--- elementary_08/elementary_08.py
+++ elementary_08/elementary_08.py
@@ -6,7 +6,6 @@ or using specified range of numbers in which there can be fibonacci numbers.
 """
 
 
-
 def fib_formula(n):
     """First way of finding fibonacci numbers - calculation formula."""
     if n <= 1:
