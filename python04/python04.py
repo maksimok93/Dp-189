07df4f4ff06bd7bd54b980b509111419a2baa5c2
Maksym Bezruchko <maksimok.dnepr@gmail.com>
2020-06-03 15:22:05 +0300

    Updated name variables in the main menu.

--- python04/python04.py
+++ python04/python04.py
@@ -59,7 +59,7 @@ def main_menu():
             break
         except:
             print("Please, input the correct integer value! ")
-    tasks = [list(map(int, input("Enter a, m, b using space: ").split())) for i in range(n)]
+    tasks = [list(map(int, input("Enter best-case, most-case and worst-case using space: ").split())) for i in range(n)]
     task_estimates = []
     task_sd = []
     for task in tasks:
