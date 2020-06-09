48d21557bc787b62f42bb6148741ac5a2f76676e
Maksym Bezruchko <maksimok.dnepr@gmail.com>
2020-06-09 11:16:34 +0300

    Added type hints to program functions.

--- python04/python04.py
+++ python04/python04.py
@@ -17,14 +17,14 @@ class Task:
         self.most_likely_case = most_likely_case
         self.worst_case = worst_case
 
-    def get_task_estimate(self):
+    def get_task_estimate(self) -> float:
         """
         Estimate is a weighted average which takes into account both the most optimistic
         and most pessimistic estimates provided.
         """
         return round((self.best_case + 4 * self.most_likely_case + self.worst_case) / 6, 0)
 
-    def get_task_sd(self):
+    def get_task_standard_deviation(self) -> float:
         """Task Standard Deviation (SD) measures the variability or uncertainty in the estimate."""
         return round((self.worst_case - self.best_case) / 6, 0)
 
@@ -33,23 +33,23 @@ class Project:
     """The Project class consists of estimable tasks, i.e. of the work breakdown structure. Estimates the expected
     value E(task) and the standard error SE(task) of this estimate for each task time."""
 
-    def __init__(self, task_estimates, task_sd):
+    def __init__(self, task_estimates: list, task_standard_deviation: list):
         self.task_estimates = task_estimates
-        self.task_sd = task_sd
+        self.task_standard_deviation = task_standard_deviation
 
-    def get_total_estimate(self):
+    def get_total_estimate(self) -> float:
         """Getting total estimate of our project."""
         return sum(self.task_estimates)
 
-    def get_standard_error(self):
+    def get_standard_error(self) -> float:
         """Getting standard error of project in this way: SE(project) = sqrt( sum(squared SD (tasks)) )."""
-        return round(math.sqrt(sum([i ** 2 for i in self.task_sd])), 0)
+        return round(math.sqrt(sum([i ** 2 for i in self.task_standard_deviation])), 0)
 
-    def get_confidence_interval(self):
+    def get_confidence_interval(self) -> str:
         """Confidence interval calculates in this way: CI(project) = E(project) ± 2 × SE(project)."""
         min = (self.get_total_estimate() - 2 * self.get_standard_error())
         max = (self.get_total_estimate() + 2 * self.get_standard_error())
-        return str(f'Projects 95% confidence interval: {round(min)} ... {round(max)} points')
+        return f'Projects 95% confidence interval: {round(min)} ... {round(max)} points'
 
 
 def main_menu():
@@ -59,19 +59,23 @@ def main_menu():
             break
         except:
             print("Please, input the correct integer value! ")
-    tasks = [list(map(int, input("Enter best-case estimate, most-likely estimate and worst-case estimate using space: ").split())) for i in range(n)]
+
+    tasks = [list(
+        map(int, input("Enter best-case estimate, most-likely estimate and worst-case estimate using space: ").split()))
+        for i in range(n)]
+
     task_estimates = []
-    task_sd = []
+    task_standard_deviation = []
     for task in tasks:
         best_case = task[0]
         most_case = task[1]
         worst_case = task[2]
         task = Task(best_case, most_case, worst_case)
         task_estimates.append(task.get_task_estimate())
-        task_sd.append(task.get_task_sd())
-        print("E(task) = ", task.get_task_estimate(), "SD(task) = ", task.get_task_sd())
+        task_standard_deviation.append(task.get_task_standard_deviation())
+        print("E(task) = ", task.get_task_estimate(), "SD(task) = ", task.get_task_standard_deviation())
 
-    project = Project(task_estimates, task_sd)
+    project = Project(task_estimates, task_standard_deviation)
     print(project.get_confidence_interval())
 
 
