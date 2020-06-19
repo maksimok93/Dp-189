10d43473cc0c0763b3542ed7891ecf83bc62d684
Maksym Bezruchko <maksimok.dnepr@gmail.com>
2020-06-19 10:17:14 +0300

    Added correct docstrings and type hints to elementary_03.py

--- elementary_03/elementary_03.py
+++ elementary_03/elementary_03.py
@@ -1,28 +1,32 @@
-"""This program calculates triangles area and displays the output in descending order of their area.
-After adding each new triangle, the program asks if the user wants to add another one."""
+"""
+This program calculates triangles area and displays the output in descending order of their area.
+After adding each new triangle, the program asks if the user wants to add another one.
+"""
+
 import math
 from operator import itemgetter
 
 
-def create_triangle_data():
-    """After user input in the format <name>, <side length>, <side length>, <side length> function creates
-    a list with the data of each triangle."""
+def create_triangle_data() -> list:
+    """
+    After user input in the format <name>, <side length>, <side length>, <side length> function creates
+    a list with the data of each triangle.
+    """
     triangle_data = []
     while True:
         sides_input = input("Input the triangle name and length of 3 sides of it using comma: ").split(', ')
         if len(sides_input) == 4:
             triangle_data.append([sides_input[0], sides_input[1], sides_input[2], sides_input[3]])
+
+            response = input("Continue ? ").lower()
+            if response != 'yes' and response != 'y':
+                break
         else:
             print("Please, enter correct values!")
-            sides_input = input("Input the triangle name and length of 3 sides of it using comma: ").split(', ')
-            triangle_data.append([sides_input[0], sides_input[1], sides_input[2], sides_input[3]])
-        response = input("Continue ? ").lower()
-        if response != 'yes' and response != 'y':
-            break
     return triangle_data
 
 
-def get_triangle_area():
+def get_triangle_area() -> dict:
     """The area of the triangle is calculated by the Heron formula."""
     global area, name
     triangle_data = create_triangle_data()
@@ -39,7 +43,7 @@ def get_triangle_area():
     return result
 
 
-def get_sorted_result():
+def get_sorted_result() -> None:
     """In the created dictionary the data of the triangles are sorted in descending order of their area."""
     result = get_triangle_area()
     for key, value in sorted(result.items(), key=itemgetter(1), reverse=True):
